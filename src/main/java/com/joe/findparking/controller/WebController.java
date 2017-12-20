4ee1f5e3ff8b2e8649968f90ce30973eeaa9e9ae
Joe <joe831216@gmail.com>
2017-12-21 00:56:52 +0800

    Add findNearbyParkingLotsDefault Service and findNearbyParkingLotsRankbyDistance Service.

--- src/main/java/com/joe/findparking/controller/WebController.java
+++ src/main/java/com/joe/findparking/controller/WebController.java
@@ -2,20 +2,35 @@ package com.joe.findparking.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class WebController {
+    private static final String GOOGLE_PLACE_API_KEY = "";
 
-    @RequestMapping(value = "/findNearbyParkingLots", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
-    public String findNearbyParkingLots() {
-        String uri = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=&radius=3000&type=parking&key=";
+    private RestTemplate restTemplate;
+    private String uri;
+    private String result;
 
-        RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
+    // 尋找附近停車場(依重要性排序)
+    @RequestMapping(value = "/findnearby/parkinglots/default", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+    public String findNearbyParkingLotsDefault(@RequestParam("location") String location, @RequestParam("radius") int radius) {
+        uri = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?language=zh-TW&type=parking&location=" + location + "&radius=" + radius + "&key=" + GOOGLE_PLACE_API_KEY;
+        restTemplate = new RestTemplate();
+        result = restTemplate.getForObject(uri, String.class);
 
         return result;
     }
 
+    // 尋找附近停車場(依距離排序)
+    @RequestMapping(value = "/findnearby/parkinglots/rankbydistance", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+    public String findNearbyParkingLotsRankbyDistance(@RequestParam("location") String location) {
+        uri = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?language=zh-TW&type=parking&rankby=distance&location=" + location + "&key=" + GOOGLE_PLACE_API_KEY;
+        restTemplate = new RestTemplate();
+        result = restTemplate.getForObject(uri, String.class);
+
+        return result;
+    }
 }
